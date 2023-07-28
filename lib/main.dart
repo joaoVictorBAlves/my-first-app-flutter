8f91f37afaff26132a66836d5b89ea827d717f77
João Victor <joaovba2002@alu.ufc.br>
2023-07-28 11:32:40 -0300

    First layout and widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-      home: Text('Hello World!'),
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My first app'),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text('Hello Ninjas'),
+        ),
+        floatingActionButton: const FloatingActionButton(
+          onPressed: null,
+          child: Text('Click'),
+        ),
+      ),
     ));
